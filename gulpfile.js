6216d2f15863fd979c803a1802f92cb50079b0f9
apetrik-mutelab <a.petrik@mute-lab.com>
2018-01-10 16:31:39 +0400

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+(function() {
+
+    var gulp = require('gulp'),
+        del = require('del'),
+        shell = require('gulp-shell'),
+        browserify = require('browserify'),
+        strictify = require('strictify'),
+        buffer = require('vinyl-buffer'),
+        ngHtml2Js = require('gulp-ng-html2js'),
+        minifyHtml = require('gulp-minify-html'),
+        source = require('vinyl-source-stream'),
+        uglify = require('gulp-uglify'),
+        sourcemaps = require('gulp-sourcemaps'),
+        jshint = require('gulp-jshint'),
+        jscs = require('gulp-jscs'),
+        concat = require('gulp-concat'),
+        strip = require('gulp-strip-comments'),
+        stylus = require('gulp-stylus'),
+        autoprefixer = require('autoprefixer-stylus'),
+        lintFiles = ['src/**/*.js', '*.js'];
+
+    gulp.task('clean', function() {
+        return del('build');
+    });
+
+    gulp.task('jshint', function() {
+        return gulp.src(lintFiles)
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish'));
+    });
+
+    gulp.task('jscs', function() {
+        return gulp.src(lintFiles)
+            .pipe(jscs())
+            .pipe(jscs.reporter());
+    });
+
+    gulp.task('lint', gulp.parallel('jshint', 'jscs'));
+
+    gulp.task('build:app:html', function() {
+        return gulp.src('src/**/*.html')
+            .pipe(minifyHtml({
+                empty: true,
+                spare: true,
+                quotes: true
+            }))
+            .pipe(ngHtml2Js({
+                moduleName: 'app',
+                rename: function(url) {
+                    return url.replace('src/', '');
+                }
+            }))
+            .pipe(concat("templates-rusline.js"))
+            .pipe(uglify())
+            .pipe(gulp.dest('build/'));
+    });
+
+    gulp.task('build:app', gulp.series('build:app:html'));
+
+    gulp.task('build:stylus:css', function() {
+        return gulp.src('src/stylus/custom.styl')
+            .pipe(stylus({
+                'use': [autoprefixer('last 2 versions')],
+                'include css': true,
+                'pretty': true
+            }))
+            .pipe(gulp.dest('build/'));
+    });
+
+    gulp.task('deploy', shell.task([process.env.DEPLOY_OXY_COMMAND]));
+
+    gulp.task('watch', function() {
+        if (process.env.DEPLOY_OXY_COMMAND) {
+            gulp.watch('src/**/*.*', gulp.series('build:app', 'deploy'));
+        } else {
+            gulp.watch('src/**/*.*', gulp.series('build:stylus:css', 'build:app'));
+        }
+    });
+
+    gulp.task('build', gulp.series(
+        gulp.parallel(
+            'clean',
+            'lint'
+        ),
+        gulp.parallel(
+            'build:app',
+            'build:stylus:css'
+        )
+    ));
+
+    gulp.task('default', gulp.series('build', 'watch'));
+
+}());
